287c6d370a62daef696db566e0a6c5470e7892f2
di-saldana <dsl42@alu.ua.es>
2024-06-14 13:19:45 +0200

    Initial Commit

---
+++ X0s/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  X0s
+//
+//  Created by Dianelys Saldaña on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ X0s/X0sApp.swift
@@ -0,0 +1,17 @@
+//
+//  X0sApp.swift
+//  X0s
+//
+//  Created by Dianelys Saldaña on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct X0sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
