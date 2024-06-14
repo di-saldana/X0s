552ba9cbfd994ff0ecd12a51d4dbcff91f92534d
di-saldana <dsl42@alu.ua.es>
2024-06-14 13:52:00 +0200

    Added: Game Assets

--- X0s/AppEntry.swift
+++ X0s/AppEntry.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct X0sApp: App {
+struct AppEntry: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartView()
         }
     }
 }
